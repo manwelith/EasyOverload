35bbee09b6ddc387376d01dc91ede72164034416
Soner A <107695479+manwelith@users.noreply.github.com>
2022-07-17 16:21:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    internal class Program
+    {
+        public void Main(string[] args)
+        {
+            Islemler();
+            Islemler("Soner", "Arı");
+            Islemler(5 , 9);
+
+        }
+        public void Islemler(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Islemler(string a , string b)
+        {
+            Console.WriteLine(a + " " + b);
+        }
+        public void Islemler()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
